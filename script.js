0af5d1848c6e581c262e59a3488981d315b58d9c
Nicholas Gustafson <nickgust@mit.edu>
2023-02-01 21:38:30 -0500

    interbranch actions test

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
-fs.writeFile("./docs/_data/text.txt", 'test', "utf8", err => {
+
+fs.writeFile("./trained-models-template/docs/_data/text.txt", 'test', "utf8", err => {
   if (err) console.log(err);
 });
